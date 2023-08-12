96d005af8c471529d0de0bfaaf51cff9383df6a7
hilarious001 <blessed5000@gmail.com>
2023-08-11 21:50:58 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Program Entry point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char h;
+
+	int g;
+
+	h = 'a';
+	g = 0;
+	while
+		(g < 10) {
+			putchar(g + '0');
+			g++;
+		}
+	while
+		(h <= 'f') {
+			putchar(h);
+			h++;
+		}
+	putchar('\n');
+	return (0);
+}
