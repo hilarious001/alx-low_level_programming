e493c181f76bbc6d9a2c3589a24d4cbb5f5fea4a
hilarious001 <blessed5000@gmail.com>
2023-08-11 21:36:56 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Program Entry point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char h;
+
+	h = 'z';
+	while
+		(h >= 'a') {
+			putchar(h);
+			h--;
+		}
+	putchar('\n');
+	return (0);
+}
