5115c8be1daf5f7799b0fa7a4d8c35d7b6410907
hilarious001 <blessed5000@gmail.com>
2023-09-14 19:45:43 -0700

    To be is to be the value of a variable

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -3,7 +3,7 @@
 /**
  * print_numbers - function that prints numbers,
  * followed by a new line.
- * separator: string to be printed between numbers
+ * @separator: string to be printed between numbers
  * @n: number of ints to print
  * Return: void
  */
